56bfe15883c869d10ae7d2c44001ede0cbcfaaa7
tangenta <tangenta@126.com>
2019-10-27 13:02:01 +0800

    add server

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/pingcap/tidb/util/codec"
+	"log"
+	"net/http"
 	"os"
 )
 
 var keyFormat = flag.String("format", "proto", "output format (go/hex/base64/proto)")
+var enableServer = flag.Bool("enable-server", false, "enable server")
 
 func main() {
 	flag.Parse()
+
+	if *enableServer {
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe(":4321", nil))
+		return
+	}
 
 	if flag.NArg() != 1 {
 		fmt.Println("usage:\nmok {flags} {key}")
@@ -17,5 +27,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	N("key", []byte(flag.Arg(0))).Expand().Print()
+	N("key", []byte(flag.Arg(0))).Print()
 }
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	keys, ok := r.URL.Query()["decode"]
+	if !ok || len(keys[0]) < 1 {
+
+		log.Println("Url Param 'key' is missing")
+		return
+	}
+	key := keys[0]
+	N("key", []byte(key)).Expand()
+
+	fmt.Fprintf(w, `{"test": "xzx"}`)
+}
+
+func findTableID(node *Node) string {
+	if node.typ == "table_id" {
+		_, tableId, _ := codec.DecodeInt(node.val)
+		return fmt.Sprintf("%d",tableId)
+	}
+	for _, v := range node.variants {
+		for _, c := range v.children {
+			return findTableID(c)
+		}
+	}
+	return ""
+}
+
+//func main() {
+//	http.HandleFunc("/", handler)
+//	log.Fatal(http.ListenAndServe(":4321", nil))
+//}
